34835102feb063dafb253d0f436ce68d15fdc786
Hei Yeung Lam <heiyeunglam@Heis-MacBook-Pro.local>
2018-11-28 14:36:21 -0800

    initial express

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://ken:KlKl266712@ds159100.mlab.com:59100/soloprojectcodesmithkl'
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoose = require('mongoose')
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+
+
+
+// Connect to db
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db)
+  .then(() => console.log('connected to mlab'))
+  .catch(err => console.log(err));
+
+
+
+
+
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+
+
+
+
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
